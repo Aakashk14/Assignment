5b7627a9d37e1d39130b5f83319e3f1abb45b8bf
Aakash Kumar <64356169+Aakashk14@users.noreply.github.com>
2022-09-05 14:16:37 +0530

    Update sort.js

--- raftlabs/sort.js
+++ raftlabs/sort.js
@@ -1,20 +1,24 @@
+const {parentPort, workerData}=require('worker_threads')
+
+const db = require('./Database/data_query')
 
 
 
-module.exports=function sortdata(data){
+async function sortedData(){
+let data = await db.getData()
+data=data.data
 
-
-    function merge(left,right){
+function merge(left,right){
     let left_index=0;
     let right_index=0;
     let result=[];
-    console.log("leftt",left,right)
-    while(left_index<left.length && right_index<right.length){
+
+    while(left_index<left.length && right_index < right.length){
+
       if(left[left_index].title < right[right_index].title){
       result.push(left[left_index])
       left_index++
       }else if(left[left_index].title>right[right_index].title){
-        console.log("truee")
       result.push(right[right_index])
       right_index++;
       }
@@ -29,14 +33,12 @@ module.exports=function sortdata(data){
     result.push(right[right_index])
     right_index++
     }
-    console.log("got res",result)
 
     return result
     }
     
     
     function sort(b){
-    console.log("enter",b)
     if(b.length<=1){
     return b
     }
@@ -44,15 +46,22 @@ module.exports=function sortdata(data){
 
     let left = sort(b.slice(0,mid))
     let right = sort(b.slice(mid,b.length))
-
      return merge(left,right)
     
 
     }
-    return sort(data)
+
+return sort(data)
+    
+  }
+
+sortedData().then((data)=>{
+
+  parentPort.postMessage({status:"done",data:JSON.stringify(data)})
+})
 
 
 
 
-}
+
 
